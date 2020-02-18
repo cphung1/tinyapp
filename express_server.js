93e2528ff4c1cea3c486731cecef358eb17e53e7
cphung1 <cphung97@gmail.com>
2020-02-18 02:43:21 +0000

    added delete function

--- express_server.js
+++ express_server.js
@@ -16,27 +16,26 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-
 // sends data to urls_index.ejs
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// enter new url page thing 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// displays all urls 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+// adds new url to databse 
 app.post("/urls", (req, res) => {
-  console.log(req.body.longURL);  // Log the POST request body to the console
-  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
   shortURL = generateRandomString()
-  
   urlDatabase[shortURL] = `http://${req.body.longURL}`;
   res.redirect(`/urls/${shortURL}`)
 });
@@ -50,8 +49,16 @@ function generateRandomString() {
   return output;
 }
 
+// redirects short url to website page
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL]
   res.redirect(longURL);
 });
+
+// deletes items 
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls')
+});
+
 
