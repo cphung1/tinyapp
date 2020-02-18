c475eb4eb335a05cb385c3f0990511e0da40187a
cphung1 <cphung97@gmail.com>
2020-02-18 23:11:14 +0000

    fixed error of username not being passed through the other views

--- express_server.js
+++ express_server.js
@@ -20,23 +20,29 @@ app.listen(PORT, () => {
 
 // sends data to urls_index.ejs
 app.get("/urls", (req, res) => {
-  let templateVars = { 
+  let templateVars = {
     urls: urlDatabase,
     username: req.cookies["username"]
   };
   res.render("urls_index", templateVars);
-
 });
 
 // page to input a new url  
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    username: req.cookies["username"]
+  };
+
+  res.render("urls_new", templateVars);
 });
 
 // displays single url on its own page 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  console.log(req.body.longURL);
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
